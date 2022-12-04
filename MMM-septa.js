82034203c3a2ae47d9aba55515e92d2947aa4263
Ben Nitkin <ben@nitkin.net>
2022-12-04 11:45:01 -0500

    Working, and looks reasonably nice. The radius operator can catch one bus at a bunch of sequential stops... might swap that for route + stop ID lookups. And adding live position would be cool too.

--- MMM-septa.js
+++ MMM-septa.js
@@ -4,6 +4,11 @@
  * By Michael Teeuw https://michaelteeuw.nl
  * MIT Licensed.
  */
+
+
+// Todo: Identify the closest station for each route
+// Identify each route in the catchment
+// Or just monitor a list of station-route pairs. Offload the hard stuff to the user.
 function get_json(url){
     var request = new XMLHttpRequest(); // a new request
     request.open("GET", url, false);
@@ -14,31 +19,93 @@ function get_json(url){
 Module.register("MMM-septa", {
     // Default module config.
     defaults: {
-        name: "SEPTA: City Hall",
+        name: "SEPTA Departures: City Hall",
         lat: 39.95249015804094,
         lon: -75.16358528523044,
 
-        radius: 0.1,
+        radius: 0.05,
         // Allowed: bus, train, trolley
         type: ["bus", "trolley", "train"],
+        showStationName: true,
+        departuresPerRoute: 3,
     },
 
-    getHeader: function () {
-        return this.config.name;
-    },
+//    getHeader: function () {
+//        if (this.config.showHeader) return this.config.name;
+//        return '';
+//    },
 
     getDom: function () {
-        var wrapper = document.createElement("div");
-        wrapper.innerHTML = this.config.name;
-        return wrapper;
+        return this.wrapper;
     },
 
     start: function () {
         // Ask the node helper to find matching stations
+        this.config.identifier = this.identifier;
         this.sendSocketNotification("MONITOR_NEARBY_DEPARTURES", this.config);
+
+        // Set up initial DOM wrapper
+        this.wrapper = document.createElement("div");
     },
 
     socketNotificationReceived: function(notification, payload) {
+        if (payload['widget-id'] != this.identifier) return;
+
         Log.log(this.name + " received a socket notification: " + notification + " - Payload: " + JSON.stringify(payload));
+        let station = this.wrapper.querySelector("#station-" + payload['id']);
+
+        if (station === null) {
+            station = document.createElement("div");
+            station.id = "station-" + payload['id'];
+            this.wrapper.appendChild(station);
+
+            if (this.config.showStationName) {
+                stationName = document.createElement("div");
+                stationName.innerHTML = payload['name'];
+                stationName.className = "align-left title bright";
+                station.appendChild(stationName);
+            }
+            let departures = document.createElement("table");
+            departures.id = "departures-" + payload['id'];
+            station.appendChild(departures);
+        }
+
+        let departures = this.wrapper.querySelector("#departures-" + payload['id']);
+        departures.innerHTML = "";
+
+        // One row per route.
+        lastRoute = null;
+        let row;
+        let departureCount = 0;
+        for (let departure of payload.departures) {
+            if (departure.route != lastRoute) {
+                lastRoute = departure.route;
+                departureCount = 0;
+
+                row = departures.insertRow();
+                let route = row.insertCell();
+                route.innerHTML = departure.route;
+                route.className = "align-left bright";
+
+                let terminus = row.insertCell();
+                terminus.innerHTML = departure.terminus;
+                terminus.className = "align-left";
+                terminus.className += " xsmall";
+            }
+
+            if (departureCount == this.config.departuresPerRoute) continue;
+            departureCount += 1;
+            let departure_time = row.insertCell();
+            departure_time.innerHTML = departure.minutes;
+            if (departureCount == 1) {
+            departure_time.className = "bright";
+            departure_time.style.width = "35px";
+            } else {
+                departure_time.className = "dim xsmall";
+                departure_time.style.width = "30px";
+            }
+        }
+
+        this.updateDom();
     },
 });
--- node_helper.js
+++ node_helper.js
@@ -9,7 +9,7 @@ const fetch = require("fetch");
 const NodeHelper = require("node_helper");
 
 async function getJSON(url) {
-    Log.log("Fetching ", url);
+    Log.log("MMM-septa fetching ", url);
     const response = await fetch(url, {headers: {"User-Agent": "Mozilla/5.0 (Node.js) MagicMirror/" + global.version}});
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
@@ -36,38 +36,42 @@ module.exports = NodeHelper.create({
     },
 
     socketNotificationReceived: function (notification, payload) {
-        Log.log("SEPTA monitor recieved", notification);
+        Log.log("MMM-septa helper recieved", notification);
         if (notification === 'MONITOR_NEARBY_DEPARTURES') {
             this.watchNewStations(payload);
+            // Special update for faster loading on refresh.
+            this.updatePushETAs();
         }
     },
 
     // Non-inherited stuff
     updatePushETAs: function() {
+        Log.log("MMM-septa is updating ETAs for " + this.stations.length + " stations");
         for (let station of this.stations) {
             // Update ETAs for each line, and publish.
             for (let departure of station.departures) {
                 // This could be fancier by hitting the SEPTA real-time APIs.
-                departure.minutes = ((departure.date - Date.now()) / 1000 / 60).toFixed;
+                departure.minutes = ((departure.date - Date.now()) / 1000 / 60).toFixed();
+                //departure.minutes = (departure.minutes / 60).toFixed() + ":" + departure.minutes % 60
             }
             this.sendSocketNotification("STATION_DEPARTURES", station);
 
             // Remove departures that've departed.
-            if (station.departure[0].minutes < 0) {
-                station.departures.shift()
+            if ((station.departures.length > 0) && ((station.departures[0].date - Date.now()) < 0)) {
+                station.departures.shift();
             }
         }
     },
 
     watchNewStations: async function (config) {
-        Log.log("Searcing for stations near:", config);
+        Log.log("MMM-septa is searcing for stations using:", config);
 
         for (const vehicle_type of config.type) {
             stop_name = {'bus': 'bus_stops',
                 'train': 'rail_stations',
                 'trolley': 'trolley_stops'}[vehicle_type];
             if (stop_name === undefined) {
-                Log.warn("Vehicle type of " + vehicle_type + " is not recognized.");
+                Log.warn("MMM-septa: Unrecognized vehicle type " + vehicle_type);
                 continue;
             }
 
@@ -77,11 +81,11 @@ module.exports = NodeHelper.create({
                 + "&radius=" + config.radius
                 + "&type="   + stop_name;
             json = await getJSON(url);
-            Log.log("Found " + json.length + " " + vehicle_type + " stations");
+            Log.log("MMM-septa discovered " + json.length + " " + vehicle_type + " stations");
             for (const station of json) {
-                // Check if the station's already present.
+                // Don't duplicate stations. This protects against multiple clients or
+                // page reloads.
                 if (this.stations.some(function(element, index, array) {
-                    Log.log(element.id + " " + station.location_id);
                     return element.id == station.location_id;
                 })) continue;
 
@@ -93,8 +97,8 @@ module.exports = NodeHelper.create({
                     'lat':  station.location_lat,
                     'lon':  station.location_lon,
                     'departures': [],
-                    // This lets multiple widgets figure out which stations are for them.
-                    'widget-name': config.name,
+                    // This tracks which widget enrolled a station
+                    'widget-id': config.identifier,
                 });
             }
         }
@@ -107,11 +111,11 @@ module.exports = NodeHelper.create({
     },
 
     updateStationArrivalsFromSEPTA: async function (station) {
-        Log.warn("Checking timetable for:", station);
         // Empty arrivals list to prepare for refresh
         station.departures = [];
         if (station.type == "bus_stops" || station.type == "trolley_stops") {
-            let url = "https://www3.septa.org/api/BusSchedules/index.php?stop_id=" + station.id;
+            // Get 30 results for buses. That covers an hour of traffic at all but the busiest stops.
+            let url = "https://www3.septa.org/api/BusSchedules/index.php?results=30&stop_id=" + station.id;
             json = await getJSON(url);
             for (const route of Object.values(json)) {
                 for (const arrival of route) {
@@ -121,14 +125,43 @@ module.exports = NodeHelper.create({
                         'date': new Date(arrival.DateCalender),
                         'minutes': 0,
                         'terminus': arrival.DirectionDesc,
-                        'route': arrival.route,
                     });
                 }
             }
         }
-        if (station.type == "rail_station") {
-            Log.error("Rail isn't supported yet!");
+        if (station.type == "rail_stations") {
+            // Poll 10 results for trains; SEPTA returns 10 in each direction.
+            let url = "https://www3.septa.org/api/Arrivals/index.php?results=10&station=" + station.name;
+            json = await getJSON(url);
+            // Traversing through the weird time/date header and list of directions:
+            // {
+            //   "Tulpehocken Departures: December 4, 2022, 9:13 am": [
+            //     {
+            //       "Northbound": [
+            //         {
+            //           "direction": "N",
+            //           ...
+            for (const weirdHeader of Object.values(json)) {
+                for (const weirdList of weirdHeader) {
+                    for (const direction of Object.values(weirdList)) {
+                        for (const arrival of direction) {
+                            station.departures.push({
+                                'trip_id': arrival.train_id,
+                                'route': arrival.line,
+                                'date': new Date(arrival.depart_time),
+                                'minutes': 0,
+                                'terminus': arrival.destination,
+                            });
+                        }
+                    }
+                }
+            }
         }
-        Log.log("Found " + station.departures.length + " departures")
+        // Sort by route, then departure time.
+        // That lets the renderer make some easy assumptions.
+        station.departures.sort((one, two) =>
+            one.route.localeCompare(two.route) ||
+            one.date - two.date);
+        Log.log("MMM-septa: found " + station.departures.length + " departures from " + station.name);
     },
 })
